743fd7ec002e83615beda894b3a56a9c2dfe9d44
Janiny <janinynobrega@gmail.com>
2025-06-17 11:05:43 -0300

    inicio do crud de product

--- Desktop/myprojectbackend/src/app.js
+++ Desktop/myprojectbackend/src/app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 // Importando as rotas
 const userRoutes = require('./routes/user.routes');
 const categoryRoutes = require('./routes/category.routes');
+const productRoutes = require('./routes/product.routes');
 
 
 app.get('/', (req, res) => {
@@ -20,6 +21,18 @@ app.get('/', (req, res) => {
 // Usando as rotas
 app.use('/v1/user', userRoutes);
 app.use('/v1/category', categoryRoutes);
+app.use('/v1/product', productRoutes);
 
+// Tratamento de rotas não encontradas (404)
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Rota não encontrada.' });
+});
+
+// Middleware de tratamento de erros geral
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Erro interno do servidor.' });
+});
 
 module.exports = app;
+
