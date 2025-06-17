d179e67949a7a6c461335ea53483f25c5287e131
Janiny <janinynobrega@gmail.com>
2025-06-17 10:01:16 -0300

    inclusão do CRUD categoria

--- Desktop/myprojectbackend/src/routes/category.routes.js
+++ Desktop/myprojectbackend/src/routes/category.routes.js
@@ -6,4 +6,17 @@ const router = express.Router();
 // Rota para obter uma lista de categorias com filtros
 router.get('/search', categoryControllers.searchCategoriesController);
 
+// Rota para obter uma categoria por ID <-- ADICIONE ESTA ROTA
+router.get('/:id', categoryControllers.getCategoryByIdController);
+
+// Rota para criar uma nova categoria
+router.post('/', categoryControllers.createCategoryController);
+
+// Rota para atualizar uma categoria existente
+router.put('/:id', categoryControllers.updateCategoryController);
+
+// Rota para deletar uma categoria existente
+router.delete('/:id', categoryControllers.deleteCategoryController);
+
+
 module.exports = router;
